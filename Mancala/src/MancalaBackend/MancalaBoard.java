733f06509bbc0baefe90a8b9e32db5b31ad47d97
Jinseok Heo <genie.h1@gmail.com>
2021-04-10 19:45:54 -0700

    PickupStones methods and a console tester

--- Mancala/src/MancalaBackend/MancalaBoard.java
+++ Mancala/src/MancalaBackend/MancalaBoard.java
@@ -1,13 +1,17 @@
 package MancalaBackend;
 
+import MancalaBackend.MancalaBoard.PitSize;
+import MancalaBackend.MancalaBoard.PlayerTypes;
+
 /***
  * MancalaBoard represents a mancala board and controls the flow of the game
  */
 public class MancalaBoard {
 
+	private static final int BOARD_SIZE = 7;
     private PlayerTypes turn = PlayerTypes.PlayerA;
-    private MancalaPit[] playerAPits = new MancalaPit[7];
-    private MancalaPit[] playerBPits = new MancalaPit[7];
+    private MancalaPit[] playerAPits = new MancalaPit[BOARD_SIZE];
+    private MancalaPit[] playerBPits = new MancalaPit[BOARD_SIZE];
 
     /***
      * Creates a new mancala board
@@ -25,12 +29,12 @@ public class MancalaBoard {
         int pitSize = 3;
         if (size == PitSize.Four) pitSize = 4;
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < BOARD_SIZE-1; i++) {
             playerAPits[i] = new MancalaPit(pitSize, false);
             playerBPits[i] = new MancalaPit(pitSize, false);
         }
-        playerAPits[6] = new MancalaPit(0, true);
-        playerBPits[6] = new MancalaPit(0, true);
+        playerAPits[BOARD_SIZE-1] = new MancalaPit(0, true);
+        playerBPits[BOARD_SIZE-1] = new MancalaPit(0, true);
 
         turn = PlayerTypes.PlayerA;
     }
@@ -41,6 +45,99 @@ public class MancalaBoard {
      */
     public PlayerTypes getTurn() {
         return turn;
+    }
+    
+    /**
+     * Get the player A's pit list.
+     * @return player A pits
+     */
+    public MancalaPit[] getPlayerAPits() {
+    	return playerAPits;
+    }
+    
+    /**
+     * Get the player B's pit list.
+     * @return player B pits
+     */
+    public MancalaPit[] getPlayerBPits() {
+    	return playerBPits;
+    }
+    
+    /**
+     * Switch the turn.
+     */
+    public void nextTurn() {
+    	turn = turn == PlayerTypes.PlayerA ? PlayerTypes.PlayerB : PlayerTypes.PlayerA;
+    }
+    
+    /**
+     * The player who has turn pick up stones from the certain pit and placing them.
+     * @param targetPit - the index of pit that the player chose
+     * @return True if the player gains free turn, false if not.
+     */
+    public boolean pickUpStones(int targetPit) {
+    	boolean freeTurn = false;
+    	MancalaPit[] playerPit = turn == PlayerTypes.PlayerA ? playerAPits : playerBPits;
+    	MancalaPit[] opponentPit = turn == PlayerTypes.PlayerA ? playerBPits : playerAPits;
+    	MancalaPit[] currentPit = playerPit;
+    	
+    	//Pull the stones
+    	MancalaStoneCluster cluster = playerPit[targetPit].pullStones();
+    	
+    	//Place the stones
+    	boolean isPlayerPit = true;
+    	boolean passed = true;
+    	
+    	while(passed) {
+    		targetPit++;
+    		//Skip the opponent mancala
+    		if(!isPlayerPit && targetPit == BOARD_SIZE-1) {
+    			targetPit++;
+    		}
+    		
+    		//Switch the side
+    		if(targetPit >= BOARD_SIZE) {
+    			targetPit = 0;
+    			isPlayerPit = !isPlayerPit;
+    			currentPit = isPlayerPit ? playerPit : opponentPit;
+    		}
+    		
+    		//Place a stones
+    		currentPit[targetPit].passStones(cluster);
+    		passed = cluster.getStoneCount() <= 0 ? false : true;
+    		
+    		//Check if the last stone was placing on the empty pit or the mancala.    		
+    		if(cluster.getStoneCount() == 0) {
+    			if(currentPit[targetPit].getContainedCluster().getStoneCount() == 1) {
+        			playerPit[BOARD_SIZE-1].addStones(opponentPit[BOARD_SIZE-2-targetPit].pullStones());
+    				playerPit[BOARD_SIZE-1].addStones(currentPit[targetPit].pullStones());
+        		}
+    			if(currentPit[targetPit].isMancala()) {
+    				freeTurn = true;
+    			}
+    		}
+    	}
+    	return freeTurn;
+    }
+    
+    /**
+     * Check the game is over or not. Game is over when the stones in pits are gone.
+     * @return True if game is over, false if not.
+     */
+    public boolean isGameOver() {
+    	boolean isAEmpty = true;
+    	boolean isBEmpty = true;
+    	for(MancalaPit p : playerAPits) {
+    		if(p.getContainedCluster().getStoneCount() != 0) {
+    			isAEmpty = false;
+    		}
+    	}
+    	for(MancalaPit p : playerBPits) {
+    		if(p.getContainedCluster().getStoneCount() != 0) {
+    			isBEmpty = false;
+    		}
+    	}
+    	return isAEmpty && isBEmpty;
     }
 
     /***
--- Mancala/src/TestMancalaConsole/MancalaConsole.java
+++ Mancala/src/TestMancalaConsole/MancalaConsole.java
@@ -1,7 +1,61 @@
 package TestMancalaConsole;
 
+import java.util.Scanner;
+
+import MancalaBackend.*;
+import MancalaBackend.MancalaBoard.PitSize;
+import MancalaBackend.MancalaBoard.PlayerTypes;
+
 public class MancalaConsole {
     public static void main(String[] args) {
         System.out.println("Mancala testing console");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Board setting: Pit size [3, 4]");
+        PitSize size = Integer.parseInt(sc.nextLine()) == 3 ? PitSize.Three : PitSize.Four;
+        
+        MancalaBoard board = new MancalaBoard(size);
+        while(!board.isGameOver()) {
+        	System.out.println("Board:");
+            printBoard(board);
+            
+            String player = getTurnStr(board.getTurn());
+            System.out.println("Turn: " + player);
+            System.out.println(player+", choose the pit.");
+            
+            char pit = Character.toLowerCase(sc.nextLine().charAt(0));
+            if(board.getTurn() == PlayerTypes.PlayerA) {
+            	pit = (char) (pit - 'a');
+            } else {
+            	pit = (char) (('f'-'a')-(pit-'a'));
+            }
+            boolean freeTurn = board.pickUpStones(pit);
+            if(!freeTurn) {
+            	board.nextTurn();
+            }
+        }
+        
+        sc.close();
+    }
+    
+    private static void printBoard(MancalaBoard board) {
+    	MancalaPit[] playerAPits = board.getPlayerAPits();
+    	MancalaPit[] playerBPits = board.getPlayerBPits();
+    	StringBuffer output = new StringBuffer();
+    	
+    	output.append("   ");
+    	for(int i = playerBPits.length-2; i >= 0; i--) {
+    		output.append(" " + playerBPits[i].getContainedCluster().getStoneCount()+" ");
+    	}
+    	output.append("\n " + playerBPits[6].getContainedCluster().getStoneCount() + " ").append(" a  b  c  d  e  f ").append(" " + playerAPits[6].getContainedCluster().getStoneCount() + " \n");
+    	output.append("   ");
+    	for(int i = 0; i < playerAPits.length-1; i++) {
+    		output.append(" " + playerAPits[i].getContainedCluster().getStoneCount()+" ");
+    	}
+    	System.out.println(output.toString());
+    }
+    
+    private static String getTurnStr(PlayerTypes t) {
+    	return t == PlayerTypes.PlayerA ? "PlayerA" : "PlayerB";
     }
 }
