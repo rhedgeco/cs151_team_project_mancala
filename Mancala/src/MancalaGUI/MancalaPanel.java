14056afcba00c17d983fea013a9b125376ec1754
rhedgeco <rhedgeco@gmail.com>
2021-05-07 16:33:19 -0700

    added initial layout test

--- Mancala/src/MancalaGUI/MancalaPanel.java
+++ Mancala/src/MancalaGUI/MancalaPanel.java
@@ -1,5 +1,8 @@
 package MancalaGUI;
 
+import MancalaGUI.Pits.MancalaPit;
+import MancalaGUI.Pits.NormalPit;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
@@ -15,6 +18,28 @@ public class MancalaPanel extends FreeLayoutPanel {
             background = ImageIO.read(getClass().getResource("Images/background.png"));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // layout testing
+        // TODO: send info to backend
+        MancalaPit mancala = new MancalaPit();
+        mancala.addActionListener( e -> System.out.println("click lol"));
+        add(mancala, 0, 0);
+
+        MancalaPit mancala2 = new MancalaPit();
+        mancala2.addActionListener( e -> System.out.println("click lol"));
+        add(mancala2, 1080-135, 0);
+
+        for (int i = 0; i < 6; i++) {
+            NormalPit pit = new NormalPit();
+            pit.addActionListener( e -> System.out.println("click lol"));
+            add(pit, 135 + (i * 135), 12);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            NormalPit pit = new NormalPit();
+            pit.addActionListener( e -> System.out.println("click lol"));
+            add(pit, 135 + (i * 135), 540 - 135 - 12);
         }
     }
 
